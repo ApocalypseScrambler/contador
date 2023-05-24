fd2c5eaf7317ca38a2f7b1290a7867976ed353fb
Luis Reyes <luisandresreyes@hotmail.com>
2023-05-23 21:21:49 -0300

    Agregamos declarativas v-show

--- js/app.js
+++ js/app.js
@@ -3,10 +3,22 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      contador: 0
+      contador: 0,
+      botones: false,
+      textoboton: "Mostrar controles"
     }
   },
   methods: {
+    mostrar() {
+      if (this.botones == true) {
+        this.botones = false
+        this.textoboton = "Mostrar controles"
+      } else {
+        this.botones = true
+        this.textoboton = "Ocultar controles"
+      }
+      
+    },
     sumar() {
       return this.contador++
     },
